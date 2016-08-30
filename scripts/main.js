0b903170cf5c06585ef6416bab563d50bfec11f6
Andre Mas <andrejohn.mas@gmail.com>
2016-08-29 23:42:34 -0400

    Added layer selector

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,34 @@ $(document).ready(function () {
 	map.addLayer(osm);
 
 
-	var layerUrl = 'http://documentation.carto.com/api/v2/viz/836e37ca-085a-11e4-8834-0edbca4b5057/viz.json';
+// 	var layerUrl = 'http://documentation.carto.com/api/v2/viz/836e37ca-085a-11e4-8834-0edbca4b5057/viz.json';
+//
+//     var vizLayer = cartodb.createLayer(map, layerUrl);
+//     vizLayer.addTo(map)
+//     .on('done', function(layer) {
+//     }).on('error', function() {
+//         console.log(error);
+//     });
+//
+//     var accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
+//     var mapboxAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+// 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+// 			'Imagery © <a href="http://mapbox.com">Mapbox</a>';
+// 	var mapboxUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+accessToken;
+//
+//     var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
+//     var streets   = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
+//
+    var baseMaps = {
+        "Open Street Map": osm,
+//        "Grayscale": grayscale,
+//        "Streets": streets
+    };
 
-    cartodb.createLayer(map, layerUrl)
-    .addTo(map)
-    .on('done', function(layer) {
-    }).on('error', function() {
-    //log the error
-    });
+    var overlayMaps = {
+        //"Sample Carto Layer": vizLayer
+    };
+
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+
 });
