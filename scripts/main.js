0e0abff391f021254eaa0000c2bd9b017f82746d
thomasRag <tomrag@mail.com>
2016-09-28 07:47:36 -0400

    added carto vis

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,21 @@
 $(document).ready(function () {
-    var map = L.map('map').setView([45.5017, -73.5673], 13);
-
-	// create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
-
-	map.setView(new L.LatLng(45.5017, -73.5673),9);
-	map.addLayer(osm);
-
-
-// 	var layerUrl = 'http://documentation.carto.com/api/v2/viz/836e37ca-085a-11e4-8834-0edbca4b5057/viz.json';
-//
-//     var vizLayer = cartodb.createLayer(map, layerUrl);
-//     vizLayer.addTo(map)
-//     .on('done', function(layer) {
-//     }).on('error', function() {
-//         console.log(error);
-//     });
-//
-//     var accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
-//     var mapboxAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-// 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-// 			'Imagery © <a href="http://mapbox.com">Mapbox</a>';
-// 	var mapboxUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+accessToken;
-//
-//     var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
-//     var streets   = L.tileLayer(mapboxUrl, {id: 'MapID', attribution: mapboxAttribution});
-//
-    var baseMaps = {
-        "Open Street Map": osm,
-//        "Grayscale": grayscale,
-//        "Streets": streets
-    };
-
-    var overlayMaps = {
-        //"Sample Carto Layer": vizLayer
-    };
-
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
-
+//  visurl = 'https://anagraph.carto.com/api/v2/viz/bd20a288-5a7d-11e6-85cb-0e3ff518bd15/viz.json';
+  var visurl = 'https://thomasragot.carto.com/api/v2/viz/a616c578-856a-11e6-864f-0ecd1babdde5/viz.json';
+  vis = cartodb.createVis('map', visurl , {
+     shareable: false,
+    // title: true,
+     description: false,
+    search: false,
+    // tiles_loader: true,
+    layer_selector: false,
+    center_lat: 45.5388,
+    center_lon: -73.6654,
+    zoom: 11,
+    no_cdn: true
+  })
+  .error(function(err) {
+    console.log(err);
+  }).done(function (vis,layers) {
+    console.log('done');
+  });
 });
