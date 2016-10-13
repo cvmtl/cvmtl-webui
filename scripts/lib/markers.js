688190342ea72f63f3fa3805b07e312be34483e6
thomasRag <tomrag@mail.com>
2016-10-13 02:43:12 -0400

    add popup and refactoring

--- scripts/lib/markers.js
+++ scripts/lib/markers.js
@@ -1,13 +1,22 @@
 const icon = require('./icon');
 const config = require('./config');
+
+function addMarkerToMap(map,item) {
+  var latlon = item.latlon;
+  var options = { icon: icon.getIcon('tree', 'orange') };
+  var marker = L.marker(latlon, options).addTo(map);
+  marker.bindPopup(item.details).openPopup();
+}
+
+function addMarkersToMap(map,items) {
+  for (var item of items) {
+    addMarkerToMap(map,item)
+  }
+}
+
 export function addToMap(map) {
-  $.get( config.baseurl + "geo/markers", function( markers ) {
-    for (var marker of markers) {
-      var latlon = marker.latlon;
-      var options = { icon: icon.getIcon('tree', 'orange') };
-      var result = L.marker(latlon, options).addTo(map);
-      console.log(result);
-    }
+  $.get( config.baseurl + "geo/markers", function( items ) {
+    addMarkersToMap(map,items);
   })
 };
 export function postMarker(event) {
