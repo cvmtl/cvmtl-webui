fde62ceef9a5610c05841da146ac8c0da170a707
thomasRag <tomrag@mail.com>
2016-10-24 21:28:34 -0400

    update projects url

--- scripts/lib/projects.js
+++ scripts/lib/projects.js
@@ -1,7 +1,7 @@
 const config = require('./config');
 
 export function display(map) {
-  $.get( config.apiBaseUrl + "projects", function( items ) {
+  $.get( config.apiBaseUrl + "projects?type=geojson", function( items ) {
      for (var item of items) {
         L.geoJson(items,{style:config.style}).addTo(map);
      }
