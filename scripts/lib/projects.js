f8d5314dfa81261a237c8a6e9d05b833d1d7ce8b
thomasRag <tomrag@mail.com>
2016-11-12 15:57:02 -0500

    add Leaflet Labels to polygon

--- scripts/lib/projects.js
+++ scripts/lib/projects.js
@@ -1,9 +1,15 @@
 const config = require('./config');
-
+var getAllUrl = config.apiBaseUrl + "projects?type=geojson";
+var corridors;
+var polygons= new Array();
 export function display(map) {
-  $.get( config.apiBaseUrl + "projects?type=geojson", function( items ) {
+  $.get( getAllUrl, function( items ) {
      for (var item of items) {
-        L.geoJson(items,{style:config.style}).addTo(map);
+      var polygon = L.geoJson(item,{style:config.style})
+      polygons.push(polygon)
+      polygon.bindLabel(item.properties.description, { noHide: true }).addTo(map);
      }
+   }).done(function () {
+        console.log('load ok');
    })
 };
