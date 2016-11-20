40f5e131e0ea4e0c8abf6744e8efa93287304c56
thomasRag <tomrag@mail.com>
2016-11-19 19:38:52 -0500

    add modal

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,17 @@ const $ = require('jquery');
 const config = require('./lib/config');
 const polylabel = require ('polylabel');
 const mapboxgl = require('mapbox-gl');
-const mapHelper = require ('./lib/map-helper.js')
+const mapHelper = require ('./lib/map-helper.js');
+const Modal = require('semantic-ui-modal');
+const Transition = require('semantic-ui-transition');
+const  Dimmer = require('semantic-ui-dimmer');
+
+$.fn.dimmer = Dimmer;
+$.fn.transition = Transition;
+$.fn.modal = Modal;
+
 $(document).ready(function () {
+
   mapboxgl.accessToken = config.mapbox.token;
   var map = new mapboxgl.Map({
       container: 'map',
@@ -11,19 +20,40 @@ $(document).ready(function () {
      zoom: config.baseZoom,
      center: [config.centerLat, config.centerLng]
   });
+
   var getAllUrl = config.apiBaseUrl + "projects?type=geojson";
-  var getAllUrl = "http://localhost:8080/projects.json";
+  if (config.devMode) {
+    var getAllUrl = "projects.json";
+  }
+
   map.on('load', function () {
-    $.get( getAllUrl, function( items ) {
-      mapHelper.addCorridors(map,items);
-    }).done(function (items) {
-          console.log(items);
-    })
-    // Create a popup, but don't add it to the map yet.
     var popup = new mapboxgl.Popup({
         closeButton: false,
         closeOnClick: false
     });
+    $.get( getAllUrl, function( items ) {
+      mapHelper.addCorridors(map,items);
+      for(var item of items){
+        var menuId = "item-"+ item.properties.id;
+        var relatedId = item.properties.id - 1
+        $('.menu').append(
+          '<a class="item" related='+ relatedId +
+          '>' +
+          item.properties.description+'</a>')
+        }
+    })
+    .done(function (items) {
+          console.log('chargement ok');
+          $('.item').on('click', function(){
+            var itemId = $(this).attr('related');
+            var item = items[itemId];
+            var p = polylabel(item.geometry.coordinates);
+            map.flyTo({
+                center: p,
+                 zoom: 12
+            });
+          });
+    })
 
     map.on('mousemove', function(e) {
         var features = map.queryRenderedFeatures(e.point, { layers: ['corridors'] });
@@ -34,17 +64,28 @@ $(document).ready(function () {
             popup.remove();
             return;
         }
-
         var feature = features[0];
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-
         popup.setLngLat(polylabel(feature.geometry.coordinates))
            .setHTML(feature.properties.description)
             .addTo(map);
-        map.setPaintProperty('corridors', 'fill-opacity', 1);
+    });
+
+    map.on('click', function(e) {
+        console.log('click');
+        var features = map.queryRenderedFeatures(e.point, { layers: ['corridors'] });
+        if (!features.length) {
+            return;
+        }
+        var feature = features[0];
+        var testresult = $('.ui.modal .header').html(feature.properties.description);
+        console.log(testresult);
+        $('.ui.modal').modal({
+          inverted: true
+        })
+        .modal('show');
     });
 
 
-});
+  });
+
 });
