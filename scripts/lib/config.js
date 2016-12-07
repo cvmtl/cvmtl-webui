4fc8eedb39b5b5fb164132e4795ca95c06d7fb4f
thomasRag <tomrag@mail.com>
2016-12-07 01:19:11 -0500

    change basemap reference

--- scripts/lib/config.js
+++ scripts/lib/config.js
@@ -3,8 +3,8 @@ export const baseZoom = 9;
 export const centerLat = -73.6654;
 export const centerLng = 45.5388;
 export const mapbox = {
-    token : 'pk.eyJ1IjoidG9tcmFnIiwiYSI6ImQ4OWZiMWQwOGM5NDNmMGIzNDhlM2I0MGI5NGM4MzFhIn0.-7KNnHCqDmjUJ-tvjngx-A',
-    style : 'mapbox://styles/tomrag/ciq8kpayl001eb0kqkl84301h'
+    token : 'pk.eyJ1IjoiY3ZtdGwiLCJhIjoiY2l2ZWEwNzBkMDBwbzJvbzB0ZWdjM2JiaSJ9.QZAmBwV1f-EYQ20GnZkJ8A',
+    style : 'mapbox://styles/cvmtl/ciw2pjpq2005v2kr32l393bs3'
 }
 export const apiBaseUrl = 'https://www.corridorsvertsmtl.org/api/'
 export const style =
